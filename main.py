149ecb27977a86ab9d09ba92bdee49567f00d8ab
Aseem Srivastava <aseemsrivastava9@gmail.com>
2023-02-10 01:53:24 +0530

    Added state names to map/gif
    Added state names to map if guessed correct by user.
    Show total number of states and total number of states guessed correctly on prompt title.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -7,9 +8,26 @@ screen.addshape(image)
 
 turtle.shape(image)
 
-# get user input
-answer_state = screen.textinput(title="Guess the state", prompt="What is the name of another state?")
-print(answer_state)
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    # get user input and convert it to title case
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 states correct",
+                                    prompt="What is the name of another state?").title()
+    print(answer_state)
+
+    # Check if user has guessed the correct state
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        #t.write(state_data.state.item())
+        t.write(answer_state)
 
 # get mouse click coordinates
 def get_mouse_click_cord(x, y):
